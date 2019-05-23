e7da327fb49c1c40048db050f1a4e8731d1c6c7e
stupendous-bookmark-8904 <r.park@yale.edu>
2019-05-23 02:16:54 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,13 @@
 # Code your solution here!
 def run_guessing_game
   loop do
-    
+    input = gets.chomp
+    num = rand(1..10)
+
+    if input == "exit"
+      puts "bye"
+      break
+    else
+      
   end
 end
